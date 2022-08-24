522320fe788c24ef24ce5311ca26852a1486bad8
adamror100 <rohret@hotmail.se>
2022-08-24 22:56:41 +0200

    Fått en stabil grund med terminal liknande fönster och har även lagt till timeouts och promises så att texten fungerar med animationer

---
+++ commands.js
@@ -0,0 +1,33 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = "mailto:forrest@fkcodes.com";
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!👋",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software",
+  "development & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creator, and it's",
+  "the best decision I ever made.",
+  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
+  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
+  "because they're reading this right now, is that I also run a creative & media agency.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>",
+];
--- index.js
+++ index.js
@@ -1,4 +1,55 @@
-console.log("Hello World");
+const termText = document.getElementById("terminalText");
+var i = 0;
+var textFinished = 0;
+var testtext = "aaaaa";
 
-let name = "hejsan";
-console.log(name);
+typingDelay(testtext);
+
+function typingDelay(text) {
+  console.log(text);
+  if (i < text.length) {
+    /*document.getElementById("mainText").innerHTML += text.charAt(i);*/
+    addLine(text.charAt(i));
+    i++;
+    setTimeout(function () {
+      typingDelay(text);
+    }, 15);
+  }
+}
+
+function enterKey(e) {
+  e = e || window.event;
+  if (e.keyCode == 13 && textFinished == 0) {
+    console.log("Knappen fungerade!");
+    writeLines(whoami);
+    termText.value = "";
+  }
+}
+
+function writeLines(name) {
+  var testi = 0;
+  var promise = Promise.resolve();
+  name.forEach(function (item, index) {
+    promise = promise.then(function () {
+      i = 0;
+      typingDelay(item);
+      textFinished = 1;
+      return new Promise(function (resolve) {
+        setTimeout(resolve, item.length * 20);
+      });
+    });
+  });
+
+  promise.then(function () {
+    textFinished = 0;
+    console.log("Loop finished.");
+  });
+}
+
+function addLine(text) {
+  const newPara = document.createElement("span");
+  const node = document.createTextNode(text);
+  newPara.appendChild(node);
+  const element = document.getElementById("mainText");
+  element.appendChild(newPara);
+}
